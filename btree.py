5cbaaaa0c3c60f474bcc1d8a7d39a89e643dc23b
Mason <clark.masonm@outlook.com>
2024-11-27 22:31:20 -0600

    Added btree and index_file and starting structure for implmentation

---
+++ btree.py
@@ -0,0 +1,7 @@
+class Btree:
+    def __init__(self) -> None:
+        pass
+    # Balance the tree to prevent unexpected behavior and reduce time complexity and improving search
+    # Insert a Key-value pair into the BTree (Implement a set holding keys to reduce time complexity so tree doesn't need to be searched through to find an existing key)
+    # Delete a key-value pair from the BTree
+    # Search for a key in the BTree and return it's value
---
+++ index_file.py
@@ -0,0 +1,14 @@
+from btree import BTree
+
+class IndexFile:
+    def __init__(self) -> None:
+        # Intialize a BTree
+        pass
+    # Create a new index file
+    # Open an existing index file
+    # Insert a new key-value pair to the BTree
+    # Delete a key-value pair from the BTree
+    # Search for a key in the BTree and return it's value
+
+
+
